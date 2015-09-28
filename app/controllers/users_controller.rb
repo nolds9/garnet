1ac15d3050b74b226da6ba0422ca3f6d168e884d
RobertAKARobin <robertgfthomas@gmail.com>
2015-09-28 16:03:09 -0400

    dank user auth created

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,53 @@
+class UsersController < ApplicationController
+
+  skip_before_action :authenticate
+
+  def sign_up
+  end
+
+  def sign_up!
+    user = User.new(
+      username: params[:username],
+      password_digest: params[:password]
+    )
+    if params[:password_confirmation] != params[:password]
+      message = "Your passwords don't match!"
+    elsif user.save
+      message = "Your account has been created!"
+    else
+      message = "Your account couldn't be created. Did you enter a unique username and password?"
+    end
+    flash[:notice] = message
+    redirect_to action: :sign_in!
+  end
+
+  def sign_in
+  end
+
+  def sign_in!
+    if session[:user]
+      @user = User.find_by(username: session[:user][:username])
+    else
+      @user = User.find_by(username: params[:username])
+    end
+    if !@user
+      message = "This user doesn't exist!"
+    elsif @user.password_digest != params[:password]
+      message = "Your password's wrong!"
+    else
+      cookies[:username] = @user.username
+      session[:user]= @user
+      message = "You're signed in, #{@user.username}!"
+    end
+    flash[:notice] = message
+    redirect_to root_url
+  end
+
+  def sign_out
+    reset_session
+    message = "You're signed out!"
+    flash[:notice] = message
+    redirect_to root_url
+  end
+
+end
