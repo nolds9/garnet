3ca89493015f9df8a8d118a9cc2183e24d44d01d
RobertAKARobin <robertgfthomas@gmail.com>
2015-09-28 16:26:14 -0400

    updated models

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,4 +1,5 @@
 class Submission < ActiveRecord::Base
   belongs_to :assignment
-  belongs_to :membership
+  belongs_to :grader, class_name: "Membership"
+  belongs_to :submitter, class_name: "Membership"
 end
