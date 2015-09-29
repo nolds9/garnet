95f5081637ea52e83d8e07d941597589010b68c3
Nick Olds <nolds9@yahoo.com>
2015-09-29 14:24:58 -0400

    Working on events server side views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,6 +5,9 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
-    @attendances = @event.attendances
+    group = @event.group
+
+    # Find all 'students' for a given group, and order them by username TODO should be last_name
+    @students = group.memberships.where(is_admin?: false).joins(:user).order('users.username')
   end
 end
