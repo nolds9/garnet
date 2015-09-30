a272ecf263291aa8dd10c4eb54769539d2672289
RobertAKARobin <robertgfthomas@gmail.com>
2015-09-30 09:07:35 -0400

    removed application.yml. weird

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,8 @@
 class Group < ActiveRecord::Base
   has_many :events
   has_many :assignments
-  has_many :memberships 
+  has_many :memberships
+  belongs_to :parent, class_name: "Group"
+  has_many :subgroups, class_name: "Group", foreign_key: "parent_id"
+
 end
