94cb7472a5108fe67df4dd8c0a096661daecd201
Jesse Shawl <jesse@jshawl.com>
2015-10-01 16:50:13 -0400

    wip broken create for observations

--- app/controllers/observations_controller.rb
+++ app/controllers/observations_controller.rb
@@ -1,4 +1,19 @@
 class ObservationsController < ApplicationController
   def index
+    @group = Group.find(params[:group_id])
+    @student = Membership.find(params[:student_id])
+    @observation = Observation.new
+  end
+  def create
+    @group = Group.find(params[:group_id])
+    @student = Membership.find(params[:membership_id])
+    @observation = Observation.new observation_params
+    if @observation.save
+      redirect_to group_student_observations_path(@group, @student)
+    end
+  end
+  private
+  def observation_params
+    params.require(:observation).permit(:body, :status)
   end
 end
