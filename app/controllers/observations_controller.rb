17f5ed90448d0770d9e99a95bde15be54ecb1cc3
John Master <john.s.master@gmail.com>
2015-10-05 17:39:06 -0400

    remove commented out code from observations controller

--- app/controllers/observations_controller.rb
+++ app/controllers/observations_controller.rb
@@ -1,9 +1,4 @@
 class ObservationsController < ApplicationController
-  # def index
-  #   @group = Group.find(params[:group_id])
-  #   @student = Membership.find(params[:student_id])
-  #   @observation = Observation.new
-  # end
   def create
     @group = Group.find(params[:group_id])
     @student = Membership.find(params[:membership_id])
