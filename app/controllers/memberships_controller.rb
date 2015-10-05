d62eee9e983d1a7c9eef69d0d5068b242b03ef00
John Master <john.s.master@gmail.com>
2015-10-05 17:36:16 -0400

    add create observation

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,5 +1,2 @@
 class MembershipsController < ApplicationController
-  def show
-    @student = Membership.find(params[:id]) #TODO: jsm moved to students controller, delete when workings
-  end
 end
