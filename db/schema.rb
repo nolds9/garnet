0700e3c0008defdf545ad571a4d1b36fc5e1f7f7
Nick Olds <nolds9@yahoo.com>
2015-10-07 12:54:29 -0400

    Pull upstream, modify schema locally

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150928194100) do
+ActiveRecord::Schema.define(version: 20151007161139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 20150928194100) do
   create_table "memberships", force: :cascade do |t|
     t.integer  "group_id"
     t.integer  "user_id"
-    t.boolean  "is_admin?"
+    t.boolean  "is_admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
