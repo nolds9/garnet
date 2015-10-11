dc65673163f0bc914ed07726757a629f920fdb65
git <git@wdidc>
2015-10-11 12:41:48 -0400

    update deploy config giles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008235423) do
+ActiveRecord::Schema.define(version: 20151007161139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,6 @@ ActiveRecord::Schema.define(version: 20151008235423) do
     t.integer  "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "title"
   end
 
   create_table "groups", force: :cascade do |t|
